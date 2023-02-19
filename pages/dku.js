b963c40aa7531de62927e85b81e7f5ee14408de6
yc444 <yc444@duke.edu>
2023-02-19 23:29:29 +0800

    first commit

---
+++ pages/dku.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h className={styles.title}>Duke Kunshan University</h>
+      <div className={styles.gray}>A University at Kunshan.</div>
+    </div>
+  )
+}
